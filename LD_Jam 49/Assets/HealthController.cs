826168344e3a1ca6e38ab7e05f7f017fe7ebba26
TriDivide <liamsmitha@gmail.com>
2021-10-02 21:10:17 +0100

    - health regen is now implemented and working

--- LD_Jam 49/Assets/HealthController.cs
+++ LD_Jam 49/Assets/HealthController.cs
@@ -6,23 +6,50 @@ using UnityEngine.UI;
 public class HealthController : MonoBehaviour {
 
     public double playerHealth = 100;
-    [SerializeField] private Text healthText;
+    public Text healthText;
 
+    private bool isStable = false;
 
     public void Update() {
         UpdateHealth();
     }
 
     public void UpdateHealth() {
-        if(playerHealth > 0) { 
-            playerHealth = playerHealth - 0.01;        
+        if (!isStable) {
+            if(playerHealth > 0) { 
+                playerHealth = playerHealth - 0.01;        
 
-            healthText.text = playerHealth.ToString("0");
+                healthText.text = playerHealth.ToString("0");
+            }
+            else {
+                playerHealth = 0;
+                healthText.text = "ah, you dead.";
+            }
         }
         else {
-            playerHealth = 0;
-            healthText.text = "ah, you dead.";
+            if(playerHealth < 100) { 
+                playerHealth = playerHealth + 0.1;        
+
+                healthText.text = playerHealth.ToString("0");
+            }
+            else {
+                playerHealth = 100;
+                healthText.text = "100";
+            } 
         }
     }
+
+    public void OnTriggerStay2D(Collider2D collision) {
+        if (collision.gameObject.name == "healthZone") {
+            isStable = true;   
+        }   
+    }
+
+    public void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.name == "healthZone") {
+            isStable = false;
+        }
+    }
+
 
 }
