3f23d791550d56f0cc194a9b3281e403c845e0ad
TriDivide <liamsmitha@gmail.com>
2021-10-02 17:47:23 +0100

    - added the passive movement state to the scientist

--- LD_Jam 49/Assets/ScientistMovement.cs
+++ LD_Jam 49/Assets/ScientistMovement.cs
@@ -9,7 +9,13 @@ public class ScientistMovement : MonoBehaviour {
     public GameObject player;
     private Vector2 moveDirection;
 
+    private bool isPassive = true;
+    private Direction lastMovementDirection;
+    public float passiveStationaryDuration = 1f;
+    public float passiveMovingDuration = 0.5f;
+    private bool doPassiveMove = true; 
 
+    private bool startPassiveMovement = true;
 
     // Update is called once per frame
     void Update() {
@@ -18,6 +24,11 @@ public class ScientistMovement : MonoBehaviour {
 
     void FixedUpdate() {
         Move();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+
+        isPassive = false;
     }
 
     void OnTriggerStay2D() {
@@ -30,10 +41,76 @@ public class ScientistMovement : MonoBehaviour {
     }
 
     void OnTriggerExit2D() {
+
+        isPassive = true;
         moveDirection = new Vector2(0, 0);
+        startPassiveMovement = true;
     }
 
     private void Move() {
-        rigidBody.velocity = new Vector2(moveDirection.x * movementSpeed, moveDirection.y * movementSpeed);
+        if (!isPassive) {
+            StopAllCoroutines();
+            rigidBody.velocity = new Vector2(moveDirection.x * movementSpeed, moveDirection.y * movementSpeed);
+            
+        }
+        else {
+            if (startPassiveMovement) {
+                startPassiveMovement = false;
+                StartCoroutine(passiveMovement());
+            }
+        }
     }
+
+    IEnumerator passiveMovement() {
+        while ( 1 > 0) {
+            float elapsedTime = 0f;
+            
+            int movement = Random.Range(0, 3);
+            Direction lmd = (Direction)movement;
+
+            while (lmd == lastMovementDirection) {
+                int m = Random.Range(0, 3);
+                print(m);
+                lmd = (Direction)m;
+                yield return null;
+            }
+
+            lastMovementDirection = lmd;
+
+            if (doPassiveMove) {
+                switch (lastMovementDirection) {
+                    case Direction.Left: { moveDirection = new Vector2(-1, 0).normalized; break; }
+                    case Direction.Right: { moveDirection = new Vector2(1, 0).normalized; break; }
+                    case Direction.Up: { moveDirection = new Vector2(0, 1).normalized; break; }
+                    case Direction.Down: { moveDirection = new Vector2(0, -1).normalized; break; }
+                }
+
+                while(elapsedTime < passiveMovingDuration) {
+                        rigidBody.velocity = new Vector2(moveDirection.x * movementSpeed, moveDirection.y * movementSpeed);
+                        elapsedTime += Time.deltaTime;
+                        yield return null;
+                }
+                doPassiveMove = false;
+                
+            }
+            else {
+                while(elapsedTime < passiveStationaryDuration) {
+                    rigidBody.velocity = new Vector2(0, 0);
+                    elapsedTime += Time.deltaTime;
+                    yield return null;
+                }
+                doPassiveMove = true;
+            }
+
+            yield return null;
+        }
+    } 
+
+}
+
+enum Direction {
+    Left = 0,
+    Right = 1,
+    Up = 2, 
+    Down = 3
 }
