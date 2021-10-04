4509ec559814651be715b76aed0ed0fbe4e21f40
TriDivide <liamsmitha@gmail.com>
2021-10-04 15:42:35 +0100

    - added sprite changes

--- LD_Jam 49/Assets/PlayerMovement.cs
+++ LD_Jam 49/Assets/PlayerMovement.cs
@@ -7,8 +7,17 @@ public class PlayerMovement : MonoBehaviour {
     public float movementSpeed = 0f;
     public Rigidbody2D rigidBody;
 
+    public Sprite stationary;
+    public Sprite left;
+    public Sprite right;
+
+    private SpriteRenderer spriteRenderer; 
+
     private Vector2 moveDirection;
 
+    void Start() {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
 
     void Update() {
         HandlePlayerInputs();
@@ -22,6 +31,18 @@ public class PlayerMovement : MonoBehaviour {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
+        if (spriteRenderer != null) {
+            if (moveX < 0) {
+                spriteRenderer.sprite = left;
+            }
+            else if (moveX > 0) {
+                spriteRenderer.sprite = right;
+            }
+            else {
+                spriteRenderer.sprite = stationary;
+            }
+        }
+        
         moveDirection = new Vector2(moveX, moveY).normalized;
     }
 
